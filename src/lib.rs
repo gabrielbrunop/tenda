7056f0b45376b71620009150b539c87dbbdcdb46
gabrielbrunop <gabrielbrunoes@gmail.com>
2024-06-24 23:17:01 -0300

    feat: add read from stdin and repl

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub struct Interpreter {}
+
+impl Interpreter {
+    pub fn new() -> Interpreter {
+        Interpreter {}
+    }
+
+    pub fn interpret(&mut self, string: String) -> String {
+        string
+    }
+}
