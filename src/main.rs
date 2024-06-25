3d3be247c6fc867b4ef0749575c571be362893b1
gabrielbrunop <gabrielbrunoes@gmail.com>
2024-06-24 23:17:01 -0300

    feat: add read from stdin and repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
+use std::io;
+use std::io::{IsTerminal, Read};
+use tenda::Interpreter;
+
+fn main() -> io::Result<()> {
+    let mut stdin = io::stdin();
+
+    if stdin.is_terminal() {
+        start_repl();
+    } else {
+        let mut buffer = String::new();
+        stdin.read_to_string(&mut buffer)?;
+
+        run_string(buffer);
+    }
+
+    Ok(())
+}
+
+fn start_repl() {
+    let mut rl = DefaultEditor::new().unwrap();
+    let mut interpreter = Interpreter::new();
+
+    loop {
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(line) if line.trim() == ".exit" => break,
+            Ok(line) => {
+                rl.add_history_entry(line.as_str()).unwrap();
+
+                let output = interpreter.interpret(line);
+                println!("{}", output);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("To exit, type .exit or press CTRL+D");
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+}
+
+fn run_string(string: String) {
+    let mut interpreter = Interpreter::new();
+    let output = interpreter.interpret(string);
+
+    println!("{}", output);
 }
