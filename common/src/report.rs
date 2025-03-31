c95e489489db156e813049b320d05f5168c6ed4d
gabrielbrunop <gabrielbrunoes@gmail.com>
2025-03-30 21:07:33 -0300

    feat: add error reporting hooking for extended customization

--- common/src/report.rs
+++ common/src/report.rs
@@ -8,4 +8,63 @@ pub trait Report {
     fn get_labels(&self) -> Vec<SourceSpan>;
     fn get_helps(&self) -> Vec<String>;
     fn get_notes(&self) -> Vec<String>;
+    fn build_report_config(&self) -> ReportConfig;
+}
+
+#[derive(Debug, Clone)]
+pub struct ReportConfig {
+    pub span: Option<SourceSpan>,
+    pub labels: Vec<SourceSpan>,
+    pub helps: Vec<String>,
+    pub notes: Vec<String>,
+    pub message: String,
+}
+
+impl ReportConfig {
+    pub fn new(
+        span: Option<SourceSpan>,
+        labels: Vec<SourceSpan>,
+        helps: Vec<String>,
+        notes: Vec<String>,
+        message: String,
+    ) -> Self {
+        Self {
+            span,
+            labels,
+            helps,
+            notes,
+            message,
+        }
+    }
+
+    pub fn span(mut self, new_span: Option<SourceSpan>) -> Self {
+        self.span = new_span;
+        self
+    }
+
+    pub fn labels(mut self, new_labels: Vec<SourceSpan>) -> Self {
+        self.labels.extend(new_labels);
+        self
+    }
+
+    pub fn helps(mut self, new_helps: Vec<String>) -> Self {
+        self.helps.extend(new_helps);
+        self
+    }
+
+    pub fn notes(mut self, new_notes: Vec<String>) -> Self {
+        self.notes.extend(new_notes);
+        self
+    }
+
+    pub fn message(mut self, new_message: String) -> Self {
+        self.message = new_message;
+        self
+    }
+}
+
+pub trait HasReportHooks {
+    fn hooks() -> &'static [fn(ReportConfig) -> ReportConfig] {
+        &[]
+    }
 }
